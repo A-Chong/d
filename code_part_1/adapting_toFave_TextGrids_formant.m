4cadf2295674acfaa458a35c2741ec4ac1c2827d
Amui Chong <amuich@gmail.com>
2018-01-25 20:44:40 -0500

    When adapting grids to FAVE included Receiver textgrids in output.

--- code_part_1/adapting_toFave_TextGrids_formant.m
+++ code_part_1/adapting_toFave_TextGrids_formant.m
@@ -18,6 +18,7 @@
 % 	ST_read_praat_textgrid.m
 % 	ST_write_praat_textgrid.m
 % 	FAVE_modify_text.m
+%   dictionary.m
 
 %% Folders for input and output files
 
@@ -30,8 +31,12 @@ origin_dir(1:3) = []; % Delete first three instances,because they are:
                       % '.','..', and '.DS_Store'
 
 % Output textgrids
-mkdir([folder 'FAVE_formatted']);
-modified_dir = [folder 'FAVE_formatted/'];
+mkdir([folder 'FAVE_formatted/'], ['FAVE_rcver/']);
+mkdir([folder 'FAVE_formatted/'], ['FAVE_giver/']);
+
+output_folders = containers.Map;
+output_folders('r') = [folder 'FAVE_formatted/FAVE_rcver/'];
+output_folders('g') = [folder 'FAVE_formatted/FAVE_giver/'];
 
 %% Main body
 %  Read each .TextGrid file, get main 4 tiers, adapt annotations to the 
@@ -40,25 +45,19 @@ modified_dir = [folder 'FAVE_formatted/'];
 for file = 1:length(origin_dir)
     
     % Read the individual .TextGrid file
-    TextGrid(file).read = ST_read_praat_textgrid([path origin_dir(file).name])
+    Grid(file).read = ST_read_praat_textgrid([path origin_dir(file).name])
     
-    % Ignore cases like TexGrid(212) because file 117 is corrupted
-    if length(TextGrid(file).read) > 1; 
+    % Ignore cases like TexGrid(212) because file 117 is corrupt
+    if length(Grid(file).read) > 1; 
         
         % Use only Gphn, Gwrd, Rphn, Rwrd tiers, and ignore the rest.
-        Rphn = TextGrid(file).read(find(strcmp({TextGrid(file).read(:).name}, 'Rphn')));
-        Rwrd = TextGrid(file).read(find(strcmp({TextGrid(file).read(:).name}, 'Rwrd')));
-        Gphn = TextGrid(file).read(find(strcmp({TextGrid(file).read(:).name}, 'Gphn')));
-        Gwrd = TextGrid(file).read(find(strcmp({TextGrid(file).read(:).name}, 'Gwrd')));
+        tiers = {'Rphn', 'Rwrd', 'Gphn', 'Gwrd'};
+        for t = 1:4;
+            index = find(strcmp({Grid(file).read(:).name}, tiers{t}));
+            TextGrid(file).read(t) = Grid(file).read(index);
+        end;        
         
-        TextGrid(file).read(1) = Rphn;
-        TextGrid(file).read(2) = Rwrd;
-        TextGrid(file).read(3) = Gphn;
-        TextGrid(file).read(4) = Gwrd;
-        
-        TextGrid(file).read(5:end) = [];
-        
-        % Dictionary check
+        % Dictionary check, call to dictionary.m
         for role = [2,4]
             words  = TextGrid(file).read(role).INT;
             phones = TextGrid(file).read(role-1).INT;
@@ -78,27 +77,33 @@ for file = 1:length(origin_dir)
             end;
         end;
         
-        % Adapt to FAVE format each of the relevant tiers
+        % Adapt each of the relevant tiers to FAVE format, and save them
+        % in dictionary named tier_book with their name as key ('Rphn','Gwrd',etc.) 
+        tier_book = containers.Map;  % -- dictionary object
         for tier = 1:4
             a = TextGrid(file).read(tier).INT;
-            name = TextGrid(file).read(tier).name;
-            TextGrid(file).read(tier).INT = Fave_modify_text(a, name);
+            tier_name = TextGrid(file).read(tier).name;
+            TextGrid(file).read(tier).INT = Fave_modify_text(a, tier_name);
+            
+            tier_book(tier_name) = TextGrid(file).read(tier);
         end;
-    
-        % Create Giver-only structure
-        Giver(file).read(1) = TextGrid(file).read(3);
-        Giver(file).read(2) = TextGrid(file).read(4);
-    
-    % Saving Giver .TextGrid file    
-    file_name = [modified_dir 'Giver_FAVE_modified_' ...
-        origin_dir(file).name(1:15) 'g.TextGrid'];    
-
-    ST_write_praat_textgrid(Giver(file).read, file_name);    
-    
+        
+        % Create and save role-specific structures
+        for role = {'r','g'};
+            switch role{1};                
+               case 'r';
+                 [read(1),read(2)] = deal(tier_book('Rphn'),tier_book('Rwrd'));
+                case 'g';
+                 [read(1),read(2)] = deal(tier_book('Gphn'),tier_book('Gwrd'));
+            end;
+            ST_write_praat_textgrid(read,[output_folders(role{1}) ...
+                origin_dir(file).name(1:15) role{1} '.TextGrid']);
+        end;
+            
 %     % Saving .TextGrid file containing both Giver AND Receiver
-%     giv_rec_filename = [modified_dir 'FAVE_modified_' ...
+%     filename = [folder 'FAVE_formatted/' ...
 %         origin_dir(file).name];
-%     ST_write_praat_textgrid(TextGrid(file).read, giv_rec_filename)
+%     ST_write_praat_textgrid(TextGrid(file).read, filename)
     
     
     end;   % if length(TextGrid(211).read) > 1;
